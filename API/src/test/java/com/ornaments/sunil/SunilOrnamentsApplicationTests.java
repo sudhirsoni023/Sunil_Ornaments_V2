da3ce1755768bd5f5ea269c2c3ef5daa2f50de33
Sudhir <sudhirsoni023@gmail.com>
2025-04-17 23:01:40 +0530

    commiting API starter template

---
+++ API/src/test/java/com/ornaments/sunil/SunilOrnamentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ornaments.sunil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunilOrnamentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
