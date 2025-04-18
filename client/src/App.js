c8c62783369c022b509cafa7fa4e82d79d12991c
Sudhir <sudhirsoni023@gmail.com>
2025-04-18 14:22:48 +0530

    front end changes

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+      <div></div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
